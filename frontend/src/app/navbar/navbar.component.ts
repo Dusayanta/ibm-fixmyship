5320d4065a6259212700bcebdfd3830459508db3
Dusayanta Prasad <dusayantaprasad@gmail.com>
2019-03-04 12:02:35 +0530

    Made a common navbar

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from "../_models/user";
+import { AuthenticationService } from "../_services/authentication.service";
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private authenticationService : AuthenticationService,
+  ) { }
 
   ngOnInit() {
   }
-
+  isLoggedIn(){
+    let currentUser:User = this.authenticationService.currentUserValue
+    if(currentUser)
+      return true;
+    else
+      return false;
+  }
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+}
 }
